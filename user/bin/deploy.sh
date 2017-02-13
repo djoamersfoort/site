48048ddad3d8357238a2afe287caa801d2f475fb
Roelof Roos <github@roelof.io>
2017-02-13 01:16:18 +0100

    Moved deploy.sh to user directory

--- user/bin/deploy.sh
+++ user/bin/deploy.sh
@@ -7,7 +7,7 @@ if [ $(whoami) = 'root' ]; then
 fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-GRAV_ROOT=$(dirname $DIR)
+GRAV_ROOT=$(dirname `dirname $DIR`)
 
 set -xe
 
@@ -15,11 +15,12 @@ cd "${GRAV_ROOT}"
 
 # Install dependancies
 echo Installing / updating Grav
-bin/gpm self-upgrade
+bin/gpm self-upgrade --all-yes
 
 # Install packages
 echo Installing admin package
-bin/gpm install --all-yes admin
+bin/gpm install --all-yes email form login admin
+bin/gpm install --all-yes sitemap
 
 # Install theme
 if [ ! -d "$GRAV_ROOT/user/themes/djo-amersfoort" ]; then
